3455762ba0c93247f18cb13528b3e3fdd973203c
Alvaro Cotumba <alvarothjg@gmail.com>
2026-04-17 14:26:58 +0200

    commit Alvaro

--- sql part/valoraciones.sql
+++ sql part/valoraciones.sql
@@ -6,3 +6,10 @@ CREATE TABLE valoraciones (
     puntuacion INT CHECK (puntuacion BETWEEN 1 AND 5),
     comentario TEXT
 );
+
+INSERT INTO valoraciones (id_usuario, id_contenido, puntuacion, comentario) VALUES
+(1, 1, 5, 'Stranger Things es genial'),
+(2, 2, 4, 'Inception muy buena'),
+(1, 2, 5, 'Peliculón de Nolan'),
+(2, 1, 5, 'Me encantó Stranger Things'),
+(1, 3, 3, 'Normalito');
